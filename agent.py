b87aeadb755bdaa85e171c9faef075e11c3b9a6c
AndreiMiculita <andreimiculita@gmail.com>
2019-09-19 17:01:50 +0200

    Added a 1d array of agents that can tell if they're satisfied with their neighbors

--- agent.py
+++ agent.py
@@ -1,3 +1,4 @@
+import random
 from enum import Enum
 
 import numpy as np
@@ -39,7 +40,7 @@ class CategoricalFeature:
 
 # When a feature is a real number
 class RealNumberFeature(object):
-    def __init__(self, value: float, difference_function, threshold):
+    def __init__(self, value: float, threshold, difference_function=lambda x: x):
         self.value = value
         self.difference_function = difference_function  # the function passed as a parameter
         self.threshold = threshold
@@ -61,12 +62,26 @@ class Agent:
     def satisfied(self, neighbors):
 
         # There's probably an even shorter way of doing this in python
-        neighbor_income_satisfaction = [self.income.preference(n) for n in neighbors]
-        neighbor_religion_satisfaction = [self.income.preference(n) for n in neighbors]
-        neighbor_ethnicity_satisfaction = [self.income.preference(n) for n in neighbors]
+        neighbor_income_satisfaction = [self.income.preference(n.income) for n in neighbors]
+        neighbor_religion_satisfaction = [self.income.preference(n.income) for n in neighbors]
+        neighbor_ethnicity_satisfaction = [self.income.preference(n.income) for n in neighbors]
 
         # 3 arrays can be treated as a matrix, np.average averages all of the numbers
         self.satisfaction = np.average([neighbor_income_satisfaction, neighbor_religion_satisfaction,
                                         neighbor_ethnicity_satisfaction])
 
         return self.satisfaction >= self.satisfaction_threshold
+
+    def __str__(self):
+        return str(id(self)) + " " + \
+               str(self.religion) + " " +\
+               str(self.ethnicity) + " " +\
+               str(self.income)
+
+
+if __name__ == "__main__":
+    agent_list = [Agent(100, 100,
+                        income=RealNumberFeature(value=random.randint(0, 100), threshold=20)) for i in range(0, 100)]
+    print([str(x) for x in agent_list])
+    for x in range(0, 99):
+        print(agent_list[x].satisfied([agent_list[x-1], agent_list[x+1]]))
