eb2badbc6a4c506533186038c056ab27e2c108be
lpraetorius <leonardpraetorius@gmail.com>
2019-10-02 16:13:25 +0200

    todo list until next wednesday

---
+++ todo.py
@@ -0,0 +1,20 @@
+#----------------TODO------------------------
+
+# Cedric
+#1. landmarks
+#   optional: 1.a incease house prices around landmark 
+
+
+# Leo
+#2. multiple categories with non-binary features
+
+
+#Nachi
+#3. change range of neighbours influencing the happiness
+#     closer neighbours have higher weight (e.g. 1/distance)
+#     get that in a 1-dimensional list(one for neighbours and one for distance)
+#       When checking neighbour satisfaction, change agent.satisfied implementation(add weights), figure out handling empty slo
+
+
+# Andrei
+#4. work on moving algorithm
